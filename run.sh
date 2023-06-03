9e724723a8abf8df473b159d779213a2940ef005
Tamnil Saito Junior <tamnil@gmail.com>
2023-06-03 04:11:44 -0300

    FIX: user folder structure

--- run.sh
+++ run.sh
@@ -8,9 +8,9 @@ docker run -it \
     --ipc host\
     --privileged \
     --volume="/dev/bus/usb:/dev/bus/usb" \
-    --volume="/home/$USER/docker-jetpack-sdk/jetson/Downloads:/home/$USER/Downloads" \
-    --volume="/home/$USER/docker-jetpack-sdk/jetson/nvidia:/home/$USER/nvidia" \
-    --volume="/home/$USER/docker-jetpack-sdk/jetson/dot-nvsdkm:/home/$USER/.nvsdkm" \
+    --volume="/home/$USER/docker-jetpack-sdk/jetson/Downloads:/home/user/Downloads" \
+    --volume="/home/$USER/docker-jetpack-sdk/jetson/nvidia:/home/user/nvidia" \
+    --volume="/home/$USER/docker-jetpack-sdk/jetson/dot-nvsdkm:/home/user/.nvsdkm" \
     --user "$(id -u):$(id -g)" \
     --env="DISPLAY=$DISPLAY" \
     --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
